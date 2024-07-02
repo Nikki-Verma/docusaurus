47d935d69232d9c072a275a25700c8858f64286e
Nikhil <lucifer@ip-192-168-5-81.ap-south-1.compute.internal>
2024-07-02 16:36:26 +0530

    updated launch sh

--- launch.sh
+++ launch.sh
@@ -7,12 +7,12 @@ then
 
   echo "Starting application on $environment environment . . ."
   echo "Exporting Variables. . . . ."
-  /usr/local/bin/serve -s dist -l 80 -C
+  /usr/local/bin/npm run serve -- --port 80
 
 else
 
   echo "Starting application on  $environment environment. . . ."
   echo "Exporting Variables. . . . ."
-  /usr/local/bin/serve -s dist -l 80 -C
+  /usr/local/bin/npm run serve -- --port 80
 
 fi
